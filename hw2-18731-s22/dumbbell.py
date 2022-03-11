f283f6a4141f0534c04b0e57a7399a03b2793740
Ubuntu <ubuntu@ip-172-31-6-168.us-west-2.compute.internal>
2022-03-11 15:02:54 +0000

    finish first part

--- hw2-18731-s22/dumbbell.py
+++ hw2-18731-s22/dumbbell.py
@@ -58,7 +58,7 @@ args = parser.parse_args()
 
 class DumbbellTopo(Topo):
     "Dumbbell topology for Shrew experiment"
-    def build(self, n=6, bw_net=100, delay='20ms', bw_host=10, maxq=100000000):
+    def build(self, n=6, bw_net=100, delay='20ms', bw_host=10, maxq=1000):
     #TODO: Add your code to create topology
 	Host1 = self.addHost( 'hl1' )
         Host2 = self.addHost( 'hl2' )
@@ -69,17 +69,15 @@ class DumbbellTopo(Topo):
         Switch1 = self.addSwitch('s1')
         Switch2 = self.addSwitch('s2')
         # Add links
-        self.addLink(Host1,Switch1,bw= bw_host,delay = delay,max_queue_size = maxq)
+        self.addLink(Host1,Switch1,bw= bw_host,delay = delay)
 
-        self.addLink(Host2,Switch1)
-        self.addLink(Host3,Switch1)
-	#self.addLink( Host1, Switch1, bw=bw_host, delay = delay, max_queue_size = maxq )
-        #self.addLink( Host2, Switch1, bw=bw_host, delay = delay, max_queue_size = maxq )
-        #self.addLink( Host3, Switch1, bw=bw_host, delay = delay, max_queue_size = maxq )
-        self.addLink( Host4, Switch2, bw=bw_host, delay = delay, max_queue_size = maxq )
-        self.addLink( Host5, Switch2, bw=bw_host, delay = delay, max_queue_size = maxq )
-        self.addLink( Host6, Switch2, bw=bw_host, delay = delay, max_queue_size = maxq )
-        self.addLink( Switch1, Switch2 )
+        self.addLink( Host1, Switch1, bw=bw_host, delay = delay)
+        self.addLink( Host2, Switch1, bw=bw_host, delay = delay)
+        self.addLink( Host3, Switch1, bw=bw_host, delay = delay)
+        self.addLink( Host4, Switch2, bw=bw_host, delay = delay)
+        self.addLink( Host5, Switch2, bw=bw_host, delay = delay)
+        self.addLink( Host6, Switch2, bw=bw_host, delay = delay)
+        self.addLink( Switch1, Switch2,bw=bw_net, delay = delay )
 		
 	
 def bbnet():
@@ -97,7 +95,11 @@ def bbnet():
     #TODO: Add your code to test reachability of hosts
     net.pingAll()
     #TODO: Add yoour code to start long lived TCP flows 
-  
+    hl1, hr1 = net.get('hl1','hr1')
+    net.iperf((hl1,hr1), port=5001) 
+
+    hl2, hr2 = net.get('hl2','hr2')
+    net.iperf((hl2,hr2), port=5002) 
     
     CLI(net)
     net.stop()
