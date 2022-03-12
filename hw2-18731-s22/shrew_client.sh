6f84cbaab7d9e4bab1d7663dec2800569ff7f060
Hao Zhang <zhanghao.jerry@example.com>
2022-03-12 08:23:08 -0500

    start task 2

--- hw2-18731-s22/shrew_client.sh
+++ hw2-18731-s22/shrew_client.sh
@@ -4,3 +4,4 @@ echo running iperf-client
 
 #TODO: add your code
 iperf -c <server ip> -p 5566 -t 15
+iperf -c -u -p 5001 -t 15 -l 1000
--- hw2-18731-s22/shrew_server.sh
+++ hw2-18731-s22/shrew_server.sh
@@ -3,3 +3,5 @@ echo set iperf-server
 
 #TODO: add your code
 iperf -s -p 5566 -i 1
+iperf -s -u -p 5001 -t 15 
+
